f1977917b5649ee1108346186e8f5aa18ee82abb
Issa-WebDev <issawebdev@gmail.com>
2025-01-12 14:08:11 +0100

    delete

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,8 @@
-// useState
 import React from "react";
-import { useState } from "react";
 
 const App = () => {
-  const [friends, setFriend] = useState(["Alex", "Huxn"]);
-
-  const addOneFriend = () => setFriend([...friends, "kone"]);
-
-  const removeOneFriend = () => setFriend(friends.filter((f) => f !== "Huxn"));
-
-  const updateFriendName = () => {
-    setFriend(friends.map((f) => (f === "Alex" ? "Alex Smith Cool" : f)));
-  };
-
-  return (
-    <div>
-      <button onClick={addOneFriend}>Add One Friend</button>
-      <button onClick={removeOneFriend}>Remove One Friend</button>
-      <button onClick={updateFriendName}>Update Friend name</button>
-
-      {friends.map((friend) => (
-        <ul key={Math.random()}>
-          <li>{friend}</li>
-        </ul>
-      ))}
-    </div>
-  );
+  return <div>App</div>;
 };
 
 export default App;
+
--- src/components/UserStatus.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const UserStatus = ({ loggedIn, isAdmin }) => {
-  return loggedIn && isAdmin ? <h1>Welcom Admin</h1> : <h1>Welcome user</h1>;
-};
-
-export default UserStatus;
